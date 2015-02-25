64b015a375ed970ae5e8accb2632afb56d3bc0ab
Nicholas Dujay <nickdujay@gmail.com>
2015-02-25 14:33:22 -0500

    made pretty test printing

--- a3/RUNALLTESTS.sh
+++ a3/RUNALLTESTS.sh
@@ -1,9 +1,21 @@
-WHERE=.
-TESTS=$WHERE/tests/
-
-
-for test in `find $TESTS -name '*.488'`
+#! /bin/bash
+WHERE=`dirname $0`
+echo "------------------------------------------------------"
+echo "$(tput setaf 2)SHOULD PASS TEST CASES:$(tput sgr0)"
+echo
+for program in `ls $WHERE/tests/passing/*.488`
 do
-    echo "Running test: $test"
-    java -jar $WHERE/dist/compiler488.jar  $test
+  echo "$(tput setaf 2)Testing: $program$(tput sgr0)"
+  ./RUNCOMPILER.sh $program
+  echo
 done
+echo "------------------------------------------------------"
+echo "$(tput setaf 2)SHOULD FAIL TEST CASES:$(tput sgr0)"
+echo
+for program in `ls $WHERE/tests/failing/*.488`
+do
+  echo "$(tput setaf 2)Testing: $program$(tput sgr0)"
+  ./RUNCOMPILER.sh $program
+  echo
+done
+echo "------------------------------------------------------"
